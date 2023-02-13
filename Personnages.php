e4e59f53da5e1fee3dd4d0084296f770c14ae478
MehdiYsl <mehdi.yansli@gmail.com>
2023-02-13 22:56:20 +0100

    Ajout des personnages du jeu, du fichier avec la class personnages et leurs carctéristiques

---
+++ Personnages.php
@@ -0,0 +1,24 @@
+<?php
+// nom de class avec majuscule
+class Personnages {
+
+    public $nom;
+    public $vie;
+    public $attaque;
+    
+
+   public function __construct($nom, $vie, $attaque)
+    {
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->attaque = $attaque;
+    }
+
+    public function ajoutVie() {
+        return $this->vie += 50;
+    }
+
+    public function armure() {
+        return $this->vie += 20;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-require 'pooGame.php';
+require 'Personnages.php';
 
+$heros = new Personnages("Mehdi", 200, 15);
+
+$ennemi1 = new Personnages("Minion", 10, 10);
+
+$ennemi2 = new Personnages("Lieutenant", 30, 30);
+
+$ennemi3 = new Personnages("Chef Minion", 100, 100);
+
+
+var_dump($heros->ajoutVie());
+var_dump($heros->armure());
